3c9942874ac57b2205cc236d50d89e820c53a471
codershona <falgunimst95@gmail.com>
2020-07-17 16:33:14 +0200

    editing

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,48 @@
+// const request = require('request')
+
+// const forecast = (latitude, longitude, callback) => {
+//     const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+
+//     request({ url, json: true }, (error, { body }) => {
+//         if (error) {
+//             callback('Unable to connect to weather service!', undefined)
+//         } else if (body.error) {
+//             callback('Unable to find location', undefined)
+//         } else {
+//             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+//         }
+//     })
+// }
+
+// module.exports = forecast
+
+
 const request = require('request')
 
-const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
-        }
-    })
+const forecast = (latitude, longitude, callback) => {
+  const url = 'http://api.weatherstack.com/current?access_key=5d603d1b7243e2be9be5afe01e214167&query=' + latitude + ',' + longitude + '&units=f'
+
+
+  // request({ url: url, json: true }, (error, response) => {
+    // request({ url, json: true }, (error, response) => {
+        request({ url, json: true }, (error, { body }) => {
+    if (error) {
+        callback('Unable to connect to weather service!!!', undefined)
+
+    // } else if (response.body.error) {
+    } else if (body.error) {
+      callback('Unabled to find location!!!!', undefined)
+
+    } else {
+
+        // callback(undefined, console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out. It feels like " + response.body.current.feelslike + " degress out."))
+        callback(undefined, console.log(body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out."))
+
+    }
+
+  })
+
 }
 
 module.exports = forecast
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,21 +1,65 @@
+// const request = require('request')
+
+// const geocode = (address, callback) => {
+//     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+
+//     request({ url, json: true }, (error, { body }) => {
+//         if (error) {
+//             callback('Unable to connect to location services!', undefined)
+//         } else if (body.features.length === 0) {
+//             callback('Unable to find location. Try another search.', undefined)
+//         } else {
+//             callback(undefined, {
+//                 latitude: body.features[0].center[1],
+//                 longitude: body.features[0].center[0],
+//                 location: body.features[0].place_name
+//             })
+//         }
+//     })
+// }
+
+// module.exports = geocode
+
+// geocode
+// Call vabk Abstraction:
+
+
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+ 
+ // const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic2F2b3J5a2l0Y2hlbiIsImEiOiJja2JveGIxeDAxZGQzMndyNTA4bWN5a25mIn0.oHH6k9SzG1rsVqT7TYbviA'
+ const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic2F2b3J5a2l0Y2hlbiIsImEiOiJja2JveGIxeDAxZGQzMndyNTA4bWN5a25mIn0.oHH6k9SzG1rsVqT7TYbviA'
+  
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to location services!', undefined)
-        } else if (body.features.length === 0) {
-            callback('Unable to find location. Try another search.', undefined)
-        } else {
-            callback(undefined, {
-                latitude: body.features[0].center[1],
-                longitude: body.features[0].center[0],
-                location: body.features[0].place_name
-            })
-        }
-    })
+// ? becomes %3F
+
+   // request({ url: url, json: true }, (error, response) => {
+    // request({ url, json: true }, (error, response) => {
+            request({ url, json: true }, (error, { body }) => {
+      if (error) {
+        callback('Unable to connect to the locations of the services!!!!', undefined)
+      // } else if (response.body.features.length === 0) {
+          } else if (body.features.length === 0) {
+        callback('Unable to find locations. Please, Try Another Search!!!', undefined)
+
+      } else {
+        callback(undefined, {
+            // latitude: response.body.features[0].center[0],
+            // longitude: response.body.features[0].center[1],
+            // latitude: response.body.features[0].center[1],
+            latitude: body.features[0].center[1],
+            // longitude: response.body.features[0].center[0],
+            longitude: body.features[0].center[0],
+            // location: response.body.features[0].place_name
+            location: body.features[0].place_name
+
+        })
+      }
+
+   })
+
 }
+
 
 module.exports = geocode
